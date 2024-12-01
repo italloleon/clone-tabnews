78452189ae6d23426f2a9e59b77325573c4eeab6
Itallo Leonardo <itallolaraujo@gmail.com>
2024-12-01 12:49:48 +0000

    add `lint:check` and `lint:fix` commands to the manifest

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um clone do site tabnews. É uma implementação do https://tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-function Home (){
-    return (
-        <div>
-            <h1>Um novo começo, boas coisas vem aí!</h1>
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <h1>Um novo começo, boas coisas vem aí!</h1>
+    </div>
+  );
 }
 
 export default Home;
